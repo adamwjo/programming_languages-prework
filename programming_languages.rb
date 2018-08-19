ba4d5a9ea02ef003cfaa29df0d9c0df956998b49
Adam Johnson <adamwjo@gmail.com>
2018-08-19 15:40:20 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,6 +5,8 @@ def reformat_languages(languages)
     value.collect do |name, attribute|
       new_hash[name] ||= attribute
       new_hash[name][:style] ||= []
+      new_hash[name][:style] << key
+      binding.pry
     end
   end
   new_hash
